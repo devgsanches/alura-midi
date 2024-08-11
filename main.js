44b3ba5c8fea97c7dc5a5b7c2bf3418d9f1b32df
Guilherme Sanches <guilhermesanchesdev@gmail.com>
2024-08-11 01:10:37 -0300

    up files

--- main.js
+++ main.js
@@ -1,57 +1,38 @@
-const teclaPom = document.querySelector('.tecla_pom')
-const teclaClap = document.querySelector('.tecla_clap')
-const teclaTim = document.querySelector('.tecla_tim')
-const teclaPuff = document.querySelector('.tecla_puff')
-const teclaSplash = document.querySelector('.tecla_splash')
-const teclaToim = document.querySelector('.tecla_toim')
-const teclaPsh = document.querySelector('.tecla_psh')
-const teclaTic = document.querySelector('.tecla_tic')
-const teclaTom = document.querySelector('.tecla_tom')
+function tocaSom(seletorAudio) {
+  const elemento = document.querySelector(seletorAudio);
 
+  if (elemento && elemento.localName === 'audio') {
+    elemento.play();
+  }
+  else {
+    console.log('Elemento não encontrado ou seletor inválido');
+  }
 
-function somTeclaPom() {
-  document.querySelector('#som_tecla_pom').play()
 }
 
-function somTeclaClap() {
-  document.querySelector('#som_tecla_clap').play()
+const listaDeTeclas = document.querySelectorAll('.tecla');
+
+
+for (let i = 0; i < listaDeTeclas.length; i++) {
+
+  const tecla = listaDeTeclas[i];
+  const instrumento = tecla.classList[1];
+  const idAudio = `#som_${instrumento}`;
+
+  tecla.onclick = function () {
+    tocaSom(idAudio);
+  }
+
+  tecla.onkeydown = function (evento) {
+
+    if (evento.code === 'Space' || evento.code === 'Enter') {
+      tecla.classList.add('ativa');
+    }
+
+  }
+
+  tecla.onkeyup = function () {
+    tecla.classList.remove('ativa');
+  }
+
 }
-
-function somTeclaTim() {
-  document.querySelector('#som_tecla_tim').play()
-}
-
-function somTeclaPuff() {
-  document.querySelector('#som_tecla_puff').play()
-}
-
-function somTeclaSplash() {
-  document.querySelector('#som_tecla_splash').play()
-}
-
-function somTeclaToim() {
-  document.querySelector('#som_tecla_toim').play()
-}
-
-function somTeclaPsh() {
-  document.querySelector('#som_tecla_psh').play()
-}
-
-function somTeclaTic() {
-  document.querySelector('#som_tecla_tic').play()
-}
-
-function somTeclaTom() {
-  document.querySelector('#som_tecla_tom').play()
-}
-
-teclaPom.addEventListener('click', somTeclaPom)
-teclaClap.addEventListener('click', somTeclaClap)
-teclaTim.addEventListener('click', somTeclaTim)
-teclaPuff.addEventListener('click', somTeclaPuff)
-teclaSplash.addEventListener('click', somTeclaSplash)
-teclaToim.addEventListener('click', somTeclaToim)
-teclaPsh.addEventListener('click', somTeclaPsh)
-teclaTic.addEventListener('click', somTeclaTic)
-teclaTom.addEventListener('click', somTeclaTom)
-
